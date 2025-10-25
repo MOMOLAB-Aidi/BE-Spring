6f4d880add23a6f1296d5302a7d435363b3ae032
벨라 <icomksh@gmail.com>
2025-10-25 19:44:21 +0900

    [FEAT/#7] 유저 엔티티 구체화

--- src/main/java/sw/momolab/server/domain/User.java
+++ src/main/java/sw/momolab/server/domain/User.java
@@ -3,38 +3,45 @@ package sw.momolab.server.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import sw.momolab.server.domain.common.BaseEntity;
+import sw.momolab.server.domain.enums.Gender;
 import sw.momolab.server.domain.enums.Role;
 import sw.momolab.server.domain.enums.UserStatus;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Users extends BaseEntity {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 메인 이메일
-    @Column(unique = true, length = 50)
-    private String primaryEmail;
-
-    // 비밀번호
-    @Column(length = 100)
-    private String password;
-
-    // 사용자 이름
+    // 이름
     @Column(nullable = false, length = 20)
     private String name;
 
-    // 사용자 활성화 여부
+    // 휴대폰번호
+    @Column(nullable = false, length = 11)
+    private String phoneNum;
+
+    // 생년월일
+    @Column(nullable = false, length = 6)
+    private LocalDate birth;
+
+    // 성별
+    @Column(nullable = false)
+    private Gender gender;
+
+    // 활성화 여부
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserStatus status;
 
-    // 사용자 권한
+    // 권한
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
---
+++ src/main/java/sw/momolab/server/domain/enums/Gender.java
@@ -0,0 +1,5 @@
+package sw.momolab.server.domain.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
