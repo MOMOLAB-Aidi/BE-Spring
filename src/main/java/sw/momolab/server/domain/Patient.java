c61c2e6521b4ade520871ab1d031abcd0db7f105
벨라 <icomksh@gmail.com>
2025-10-30 11:33:09 +0900

    [FEAT/#7] 환자 엔티티 설정

--- src/main/java/sw/momolab/server/domain/Patient.java
+++ src/main/java/sw/momolab/server/domain/Patient.java
@@ -3,47 +3,23 @@ package sw.momolab.server.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import sw.momolab.server.domain.common.BaseEntity;
-import sw.momolab.server.domain.enums.Gender;
-import sw.momolab.server.domain.enums.Role;
-import sw.momolab.server.domain.enums.UserStatus;
 
-import java.time.LocalDate;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Users extends BaseEntity {
+public class Patient extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 이름
-    @Column(nullable = false, length = 20)
-    private String name;
+    @Column(unique = true, length = 50, nullable = false)
+    private String loginId;
 
-    // 휴대폰번호
-    @Column(nullable = false, length = 11)
-    private String phoneNum;
-
-    // 생년월일
-    @Column(nullable = false, length = 6)
-    private LocalDate birth;
-
-    // 성별
-    @Column(nullable = false)
-    private Gender gender;
-
-    // 활성화 여부
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private UserStatus status;
-
-    // 권한
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role;
+    @Column(length = 100, nullable = false)
+    private String password;
 
 }
