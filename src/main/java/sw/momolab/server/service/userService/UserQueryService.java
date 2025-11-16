cd6c58af1fd82eadbb0fad7976e4713ad5173eff
벨라 <icomksh@gmail.com>
2025-11-16 18:00:23 +0900

    [FEAT/#31] 중복 쿼리 제거

--- src/main/java/sw/momolab/server/service/userService/UserQueryService.java
+++ src/main/java/sw/momolab/server/service/userService/UserQueryService.java
@@ -3,6 +3,5 @@ package sw.momolab.server.service.userService;
 import sw.momolab.server.web.dto.UserDTO.UserResponseDTO;
 
 public interface UserQueryService {
-    String getRecordDate(Long userId);
     UserResponseDTO.MyPageDTO getMyPage(Long userId);
 }
--- src/main/java/sw/momolab/server/service/userService/UserQueryServiceImpl.java
+++ src/main/java/sw/momolab/server/service/userService/UserQueryServiceImpl.java
@@ -2,13 +2,13 @@ package sw.momolab.server.service.userService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sw.momolab.server.apiPayload.code.status.ErrorStatus;
 import sw.momolab.server.apiPayload.exception.UserHandler;
 import sw.momolab.server.converter.UserConverter;
 import sw.momolab.server.domain.User;
 import sw.momolab.server.repository.RecordRepository;
 import sw.momolab.server.repository.UserRepository;
-import sw.momolab.server.web.dto.AuthDTO.AuthRequestDTO;
 import sw.momolab.server.web.dto.UserDTO.UserResponseDTO;
 
 import java.time.LocalDate;
@@ -22,18 +22,7 @@ public class UserQueryServiceImpl implements UserQueryService {
     private final UserRepository userRepository;
 
     @Override
-    public String getRecordDate(Long userId) {
-        // 사용자가 처음으로 일기를 작성한 날짜 조회
-        LocalDate firstRecordDate = recordRepository.findFirstRecordDateByUserId(userId)
-                .orElse(LocalDate.now()); // 작성 기록이 없으면 오늘 날짜를 기본값으로 사용
-
-        // 오늘 날짜와의 차이를 계산
-        long days = ChronoUnit.DAYS.between(firstRecordDate, LocalDate.now());
-
-        return "D+" + days;
-    }
-
-    @Override
+    @Transactional(readOnly = true)
     public UserResponseDTO.MyPageDTO getMyPage(Long userId) {
 
         User user = userRepository.findById(userId)
@@ -43,7 +32,8 @@ public class UserQueryServiceImpl implements UserQueryService {
         LocalDate firstRecordDate = recordRepository.findFirstRecordDateByUserId(userId)
                 .orElse(LocalDate.now()); // 작성 기록이 없으면 오늘 날짜를 기본값으로 사용
 
-        String dPlusPeriod = getRecordDate(userId); // 기록을 작성해온 기간
+        long days = ChronoUnit.DAYS.between(firstRecordDate, LocalDate.now());
+        String dPlusPeriod = "D+" + days;
 
         return UserConverter.toMyPageDTO(user, firstRecordDate, dPlusPeriod);
     }
