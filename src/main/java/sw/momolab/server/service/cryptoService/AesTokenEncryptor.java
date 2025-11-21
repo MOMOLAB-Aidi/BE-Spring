2f71132b85db0757d16a041d04f09e357c1616d3
벨라 <icomksh@gmail.com>
2025-11-21 13:09:59 +0900

    [REFACTOR/#26] 토큰 복호화 로직 삭제

--- src/main/java/sw/momolab/server/service/cryptoService/AesTokenEncryptor.java
+++ src/main/java/sw/momolab/server/service/cryptoService/AesTokenEncryptor.java
@@ -15,7 +15,6 @@ public class AesTokenEncryptor {
     @Value("${token.aes-secret-key}")
     private String secretKey;
     private Cipher encryptCipher;
-    private Cipher decryptCipher;
 
     // 클래스 초기화 시 Cipher 인스턴스 설정
     @PostConstruct
@@ -29,11 +28,11 @@ public class AesTokenEncryptor {
         // Cipher 인스턴스 초기화 (AES/ECB/PKCS5Padding 사용)
         // ECB 모드는 보안에 취약할 수 있으므로, 실제 서비스에서는 CBC 또는 GCM 모드를 권장하며, 이 경우 IV(Initialization Vector) 관리가 필요
         this.encryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        this.decryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        Cipher decryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 
         // Cipher를 암호화 모드와 복호화 모드로 초기화
         this.encryptCipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-        this.decryptCipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        decryptCipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
     }
 
     // 토큰 암호화
@@ -45,20 +44,6 @@ public class AesTokenEncryptor {
             return Base64.getEncoder().encodeToString(encrypted);
         } catch (Exception e) {
             throw new RuntimeException("Refresh Token 암호화 실패", e);
-        }
-    }
-
-    // 토큰 복호화
-    public String decrypt(String encryptedText) {
-        if (encryptedText == null) return null;
-        try {
-            // Base64로 디코딩
-            byte[] decoded = Base64.getDecoder().decode(encryptedText);
-            // 복호화 수행
-            byte[] original = decryptCipher.doFinal(decoded);
-            return new String(original, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            throw new RuntimeException("Refresh Token 복호화 실패", e);
         }
     }
 }
