ea9d3d38a5b0fb1d1bad30247a6fb11634b44a0c
벨라 <icomksh@gmail.com>
2025-11-21 14:52:02 +0900

    [REFACTOR/#26] AES 제거 및 HMAC 적용

--- src/main/java/sw/momolab/server/util/AesTokenEncryptor.java
+++ src/main/java/sw/momolab/server/util/AesTokenEncryptor.java
@@ -4,7 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.Cipher;
+import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
@@ -14,30 +14,34 @@ public class AesTokenEncryptor {
 
     @Value("${token.aes-secret-key}")
     private String secretKey;
-    private SecretKeySpec secretKeySpec;
+    private SecretKeySpec macKeySpec;
 
-    // 클래스 초기화 시 Cipher 인스턴스 설정
+    // 클래스 초기화 시 AES 키 스펙 설정
     @PostConstruct
     public void init() {
+        if (secretKey == null || secretKey.isBlank()) {
+            throw new IllegalStateException("token.aes-secret-key 설정이 비어 있습니다.");
+        }
+
         // 비밀 키를 바이트 배열로 변환
         byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
 
         if (keyBytes.length != 16 && keyBytes.length != 24 && keyBytes.length != 32) {
             throw new IllegalStateException("token.aes-secret-key 는 16/24/32 바이트여야 합니다.");
         }
-        // SecretKeySpec 생성
-        this.secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+
+        // MacKeySpec 생성
+        this.macKeySpec = new SecretKeySpec(keyBytes, "HmacSHA256");
     }
 
-    // 토큰 암호화
+    // 토큰 HMAC(일방향) 생성
     public String encrypt(String text) {
         if (text == null) return null;
         try {
-            // 매번 새로운 Cipher 인스턴스 생성 (스레드 세이프)
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);  // 저장된 키 스펙 사용
-            byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
-            return Base64.getEncoder().encodeToString(encrypted);
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(macKeySpec);
+            byte[] hmac = mac.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hmac);
         } catch (Exception e) {
             throw new RuntimeException("Refresh Token 암호화 실패", e);
         }
